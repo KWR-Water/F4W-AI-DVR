7c47249723746f1fafaa3c7784d5ace72239fb4f
seshasi@nwg.local <siddharth.seshan@kwrwater.nl>
2022-06-28 10:58:33 +0200

    Committing the sensor_metadata file

--- ai_data_validation_reconciliation/src/data/metadata/__init__.py
+++ ai_data_validation_reconciliation/src/data/metadata/__init__.py
@@ -3,6 +3,6 @@ from os.path import join
 
 path_metadata = pathlib.Path(__file__).parent.absolute()
 
-file_metadata_limits = 'sensor_metadata.json'
+file_metadata_limits = 'sensor_metadata.json.json'
 
 fpath_metadata_limits = join(path_metadata, file_metadata_limits)
